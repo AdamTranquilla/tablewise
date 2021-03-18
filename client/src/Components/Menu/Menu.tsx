48b2b4d1db02543fdbfaacc3e7b158bd7c88f381
adam <adam.tranquilla@gmail.com>
2021-03-18 14:09:12 -0600

    error with apollo

--- client/src/Components/Menu/Menu.tsx
+++ client/src/Components/Menu/Menu.tsx
@@ -7,35 +7,8 @@ import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-// import Typography from "@material-ui/core/Typography";
-// import Box from "@material-ui/core/Box";
-
-// interface TabPanelProps {
-//   children?: React.ReactNode;
-//   dir?: string;
-//   index: any;
-//   value: any;
-// }
-
-// function TabPanel(props: TabPanelProps) {
-//   const { children, value, index, ...other } = props;
-
-//   return (
-//     <div
-//       role="tabpanel"
-//       hidden={value !== index}
-//       id={`full-width-tabpanel-${index}`}
-//       aria-labelledby={`full-width-tab-${index}`}
-//       {...other}
-//     >
-//       {value === index && (
-//         <Box p={3}>
-//           <Typography>{children}</Typography>
-//         </Box>
-//       )}
-//     </div>
-//   );
-// }
+import { graphql } from "react-apollo";
+import { GET_SECTIONS } from "../../gql/section";
 
 function a11yProps(index: any) {
   return {
@@ -51,10 +24,7 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-export default function Menu() {
-  //load menu here
-  //load sections here
-
+export default function Menu(props: any) {
   const classes = useStyles();
   const theme = useTheme();
   const [value, setValue] = React.useState(0);
@@ -66,9 +36,6 @@ export default function Menu() {
   const handleChangeIndex = (index: number) => {
     setValue(index);
   };
-
-  // const [selectedSection, setSection] = useState("Apps");
-
   const sections: string[] = ["Apps", "Mains", "Deserts", "Drinks"];
 
   return (
@@ -84,8 +51,8 @@ export default function Menu() {
             variant="fullWidth"
             aria-label="full width tabs example"
           >
-            {sections.map((section) => {
-              return <Tab label={section} {...a11yProps(0)} />;
+            {sections.map((section, index) => {
+              return <Tab label={section} {...a11yProps(index)} />;
             })}
           </Tabs>
         </AppBar>
