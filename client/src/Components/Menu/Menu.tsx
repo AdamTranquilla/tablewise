d02011b222eed2666444503e8303db6089387a00
adam <adam.tranquilla@gmail.com>
2021-03-18 00:06:25 -0600

    accordion styling and functions reorganized

--- client/src/Components/Menu/Menu.tsx
+++ client/src/Components/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React /* , { useState } */ from "react";
 import Section from "./MenuSection";
 import "./Menu.css";
 
@@ -7,35 +7,35 @@ import { makeStyles, Theme, useTheme } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
+// import Typography from "@material-ui/core/Typography";
+// import Box from "@material-ui/core/Box";
 
-interface TabPanelProps {
-  children?: React.ReactNode;
-  dir?: string;
-  index: any;
-  value: any;
-}
+// interface TabPanelProps {
+//   children?: React.ReactNode;
+//   dir?: string;
+//   index: any;
+//   value: any;
+// }
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+// function TabPanel(props: TabPanelProps) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`full-width-tabpanel-${index}`}
+//       aria-labelledby={`full-width-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box p={3}>
+//           <Typography>{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// }
 
 function a11yProps(index: any) {
   return {
--- client/src/Components/Menu/MenuSectionCat.tsx
+++ client/src/Components/Menu/MenuSectionCat.tsx
@@ -1,56 +1,12 @@
 import React from "react";
 import Item from "./MenuSectionCatItem";
 import "./Menu.css";
-import { withStyles } from "@material-ui/core/styles";
-import MuiAccordion from "@material-ui/core/Accordion";
-import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
-import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import { Accordion, AccordionBtn, AccordionContent } from "./Accordions";
 
 interface CategoryProps {
   name: string;
   id?: string;
 }
-
-const Accordion = withStyles({
-  root: {
-    border: "1px solid rgba(0, 0, 0, .125)",
-    boxShadow: "none",
-    "&:not(:last-child)": {
-      borderBottom: 0,
-    },
-    "&:before": {
-      display: "none",
-    },
-    "&$expanded": {
-      margin: "auto",
-    },
-  },
-  expanded: {},
-})(MuiAccordion);
-
-const AccordionBtn = withStyles({
-  root: {
-    backgroundColor: "rgba(0, 0, 0, .03)",
-    borderBottom: "1px solid rgba(0, 0, 0, .125)",
-    marginBottom: -1,
-    minHeight: 56,
-    "&$expanded": {
-      minHeight: 56,
-    },
-  },
-  content: {
-    "&$expanded": {
-      margin: "12px 0",
-    },
-  },
-  expanded: {},
-})(MuiAccordionSummary);
-
-const AccordionContent = withStyles((theme) => ({
-  root: {
-    padding: theme.spacing(2),
-  },
-}))(MuiAccordionDetails);
 
 const items = [
   {
@@ -100,7 +56,7 @@ export default function Category({ name, id }: CategoryProps) {
   };
 
   return (
-    <Accordion square onChange={handleChange(`panel${id}`)}>
+    <Accordion square onChange={handleChange("id")}>
       <AccordionBtn aria-controls="panel${id}-content" id={id}>
         <h3 style={{ margin: 0, marginBottom: 5 }}>{name}</h3>
       </AccordionBtn>
