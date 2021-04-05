aa8908e0587db6be9c19861735a4a808e0656213
adam <adam.tranquilla@gmail.com>
2021-04-04 21:33:21 -0600

    ts-ignore - uuid

--- client/src/Components/Menu/MenuSectionCatItem.tsx
+++ client/src/Components/Menu/MenuSectionCatItem.tsx
@@ -11,8 +11,8 @@ import { OrderContext } from "../../context/Order";
 import socket from "../../utils/socket.io.js";
 // @ts-ignore
 import Modal from "react-modal";
-// @ts-ignore
 import SplitTable from "./SplitTable";
+// @ts-ignore
 import { v4 as uuid } from "uuid";
 import { ADD_TO_CART } from "../../graphql/cart";
 import { useMutation } from "@apollo/client";
