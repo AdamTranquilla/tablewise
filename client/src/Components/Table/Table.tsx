cdcd76492889116eb16d861d871eebf2b38b8316
adam <adam.tranquilla@gmail.com>
2021-04-05 19:07:19 -0600

    seats organized to serving best practice

--- client/src/Components/Table/Table.tsx
+++ client/src/Components/Table/Table.tsx
@@ -9,40 +9,21 @@ interface TableProps {
   onClick?: (seatId: number) => void;
 }
 
-export default function Table({
-  mySeat,
-  filledSeats,
-  totalSeats,
-  onClick,
-}: TableProps) {
+export default function Table({ filledSeats, onClick }: TableProps) {
   return (
     <div id="table-container">
       <div className="table-row">
-        <TableSeat
-          isFilled={filledSeats.indexOf(0) !== -1}
-          alignment="center"
-          onClick={onClick}
-          id={0}
-        />
         <TableSeat
           isFilled={filledSeats.indexOf(1) !== -1}
           alignment="center"
           onClick={onClick}
           id={1}
         />
-      </div>
-      <div className="table-row">
         <TableSeat
           isFilled={filledSeats.indexOf(2) !== -1}
-          alignment="flex-start"
+          alignment="center"
           onClick={onClick}
           id={2}
-        />
-        <TableSeat
-          isFilled={filledSeats.indexOf(3) !== -1}
-          alignment="flex-end"
-          onClick={onClick}
-          id={3}
         />
       </div>
       <div className="table-row">
@@ -53,10 +34,10 @@ export default function Table({
           id={4}
         />
         <TableSeat
-          isFilled={filledSeats.indexOf(5) !== -1}
+          isFilled={filledSeats.indexOf(3) !== -1}
           alignment="center"
           onClick={onClick}
-          id={5}
+          id={3}
         />
       </div>
     </div>
